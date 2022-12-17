5806b508ca62cf1a3e12d9d48da19268b1ca75f5
Azizxon <90305397+saydaliyev1996@users.noreply.github.com>
2022-12-17 10:19:45 +0500

    barbershop landing page

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+ menu.onscroll = () => {
+ menu.classList.remove('fa-times');
+ navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".review-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+    },
+});
